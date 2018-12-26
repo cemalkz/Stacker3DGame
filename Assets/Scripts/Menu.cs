7cc584d4fb1fabe48cf2fd980d570bc3191496e5
Furkan Hattapoğlu <furkan.hattapoglu@ogr.sakarya.edu.tr>
2018-12-26 15:28:03 +0300

    Proje dosyaları eklendi

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Text textHigh;
+    public int highScore;
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("highscore"))
+        {
+            PlayerPrefs.SetInt("highscore", 0);
+            PlayerPrefs.Save();
+        }
+        textHigh.text = PlayerPrefs.GetInt("highscore").ToString();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
